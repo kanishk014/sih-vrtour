7c26cc4b8c728ca87c1ce6b8840dc5104278377d
kanishk <kanishk.9871@gmail.com>
2022-03-28 18:29:43 +0530

    Updates

--- src/store/actions/propertiesAction.js
+++ src/store/actions/propertiesAction.js
@@ -19,7 +19,7 @@ import {
     GET_PROPDETAILS_SUCCESS,
     GET_PROPDETAILS_FAIL,
 } from "../constants/constants";
-const API_URL = "https://vrdoor-server.herokuapp.com";
+const API_URL = "http://localhost:4000";
 
 export const addProperty = (details) => async (dispatch, getState) => {
 	console.log("action");
