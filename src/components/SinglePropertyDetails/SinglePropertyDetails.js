60c335156efb7dd3cfb516a5b8aec42a41cb1bf4
dudenayak <deveshjain2k1@gmail.com>
2022-08-26 09:27:34 +0530

    CSS Fixed

--- src/components/SinglePropertyDetails/SinglePropertyDetails.js
+++ src/components/SinglePropertyDetails/SinglePropertyDetails.js
@@ -19,7 +19,14 @@ import 'react-phone-number-input/style.css';
 import PhoneInput from 'react-phone-number-input';
 import { useReactToPrint } from 'react-to-print';
 import { useNavigate } from 'react-router-dom';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
+import { Datepicker, Page, getJson, setOptions } from '@mobiscroll/react';
 import axios from 'axios';
+
+setOptions({
+  theme: 'ios',
+  themeVariant: 'light',
+});
 
 const SingleProperty = () => {
   const componentRef = useRef();
@@ -117,6 +124,133 @@ const SingleProperty = () => {
 
   const selectChange = (event) => {
     setNationality(event.target.value);
+  };
+
+  // example for today's labels and invalids
+  const [multiple, setMultiple] = React.useState([
+    '2022-08-11T00:00',
+    '2022-08-16T00:00',
+    '2022-08-17T00:00',
+  ]);
+  const min = '2022-08-26T00:00';
+  const max = '2023-02-26T00:00';
+  const [singleLabels, setSingleLabels] = React.useState([]);
+  const [singleInvalid, setSingleInvalid] = React.useState([]);
+  const [datetimeLabels, setDatetimeLabels] = React.useState([]);
+  const [datetimeInvalid, setDatetimeInvalid] = React.useState([]);
+  const [multipleLabels, setMultipleLabels] = React.useState([]);
+  const [multipleInvalid, setMultipleInvalid] = React.useState([]);
+
+  const onPageLoadingSingle = React.useCallback((event, inst) => {
+    getPrices(event.firstDay, (bookings) => {
+      setSingleLabels(bookings.labels);
+      setSingleInvalid(bookings.invalid);
+    });
+  }, []);
+
+  const onPageLoadingDatetime = React.useCallback((event, inst) => {
+    getDatetimes(event.firstDay, (bookings) => {
+      setDatetimeLabels(bookings.labels);
+      setDatetimeInvalid(bookings.invalid);
+    });
+  }, []);
+
+  const onPageLoadingMultiple = React.useCallback((event, inst) => {
+    getBookings(event.firstDay, (bookings) => {
+      setMultipleLabels(bookings.labels);
+      setMultipleInvalid(bookings.invalid);
+    });
+  }, []);
+
+  const getPrices = (d, callback) => {
+    let invalid = [];
+    let labels = [];
+
+    getJson(
+      'https://trial.mobiscroll.com/getprices/?year=' +
+        d.getFullYear() +
+        '&month=' +
+        d.getMonth(),
+      (bookings) => {
+        for (let i = 0; i < bookings.length; ++i) {
+          const booking = bookings[i];
+          const d = new Date(booking.d);
+
+          if (booking.price > 0) {
+            labels.push({
+              start: d,
+              title: '$' + booking.price,
+              textColor: '#e1528f',
+            });
+          } else {
+            invalid.push(d);
+          }
+        }
+        callback({ labels: labels, invalid: invalid });
+      },
+      'jsonp'
+    );
+  };
+
+  const getDatetimes = (d, callback) => {
+    let invalid = [];
+    let labels = [];
+
+    getJson(
+      'https://trial.mobiscroll.com/getbookingtime/?year=' +
+        d.getFullYear() +
+        '&month=' +
+        d.getMonth(),
+      (bookings) => {
+        for (let i = 0; i < bookings.length; ++i) {
+          const booking = bookings[i];
+          const bDate = new Date(booking.d);
+
+          if (booking.nr > 0) {
+            labels.push({
+              start: bDate,
+              title: booking.nr + ' SPOTS',
+              textColor: '#e1528f',
+            });
+            invalid = [...invalid, ...booking.invalid];
+          } else {
+            invalid.push(d);
+          }
+        }
+        callback({ labels: labels, invalid: invalid });
+      },
+      'jsonp'
+    );
+  };
+
+  const getBookings = (d, callback) => {
+    let invalid = [];
+    let labels = [];
+
+    getJson(
+      'https://trial.mobiscroll.com/getbookings/?year=' +
+        d.getFullYear() +
+        '&month=' +
+        d.getMonth(),
+      (bookings) => {
+        for (let i = 0; i < bookings.length; ++i) {
+          const booking = bookings[i];
+          const d = new Date(booking.d);
+
+          if (booking.nr > 0) {
+            labels.push({
+              start: d,
+              title: booking.nr + ' SPOTS',
+              textColor: '#e1528f',
+            });
+          } else {
+            invalid.push(d);
+          }
+        }
+        callback({ labels: labels, invalid: invalid });
+      },
+      'jsonp'
+    );
   };
 
   const bookTicketCall = () => {
@@ -289,6 +423,43 @@ const SingleProperty = () => {
                     <Modal.Body>
                       <div>
                         <div ref={componentRef} className='add-property'>
+                          {/* integrating calender to book slots */}
+                          <Page className='md-calendar-booking'>
+                            <div className='mbsc-form-group'>
+                              <div className='mbsc-form-group-title'>
+                                Single date & appointment booking
+                              </div>
+                              <Datepicker
+                                display='inline'
+                                controls={['calendar']}
+                                min={min}
+                                max={max}
+                                labels={singleLabels}
+                                invalid={singleInvalid}
+                                pages='auto'
+                                onPageLoading={onPageLoadingSingle}
+                              />
+                            </div>
+                            <div className='mbsc-form-group'>
+                              <div className='mbsc-form-group-title'>
+                                Select date & time
+                              </div>
+                              <Datepicker
+                                display='inline'
+                                controls={['calendar']}
+                                min={min}
+                                max={max}
+                                minTime='08:00'
+                                maxTime='19:59'
+                                stepMinute={60}
+                                width={null}
+                                labels={datetimeLabels}
+                                invalid={datetimeInvalid}
+                                onPageLoading={onPageLoadingDatetime}
+                                cssClass='booking-datetime'
+                              />
+                            </div>
+                          </Page>
                           <h1>Book Tickets</h1>
                           <form
                             method='post'
@@ -387,15 +558,80 @@ const SingleProperty = () => {
                           </form>
                         </div>
                       </div>
+
+                      {/* print */}
+                      {/* <div
+                        ref={componentRef}
+                        className='add-property'
+                        style={{ display: 'none' }}
+                      >
+                        <h1>Book Tickets</h1>
+                        <form
+                          method='post'
+                          // onSubmit={handleSubmit}
+                        >
+                          <div className='form-input'>
+                            <h4>Name:</h4>
+                            <input
+                              type='text'
+                              name='name'
+                              placeholder='Enter name'
+                              required
+                              value={booking.name}
+                              onChange={handleChange}
+                            />
+                          </div>
+                          <div className='form-input'>
+                            <h4>Date: </h4>
+                            <input
+                              type='date'
+                              name='date'
+                              placeholder='Enter date of visiting'
+                              required
+                              value={booking.date}
+                              onChange={handleChange}
+                            />
+                          </div>
+                          <div className='form-input'>
+                            <h4>Mobile Number:</h4>
+                            <PhoneInput
+                              placeholder='Enter phone number'
+                              name='mobileNo'
+                              required
+                              value={phone}
+                              onChange={setPhone}
+                            />
+                          </div>
+                          <div className='form-input'>
+                            <h4>Number of Person:</h4>
+                            <input
+                              type='number'
+                              name='noOfPersons'
+                              placeholder='Number of person visiting'
+                              min='0'
+                              required
+                              value={booking.noOfPersons}
+                              onChange={handleChange}
+                              onInput={personChange}
+                            />
+                          </div>
+                          <div className='form-input'>
+                            <h4>Nationality:</h4>
+                            <select value={nationality} onChange={selectChange}>
+                              <option value='none' selected disabled hidden>
+                                Select nationality
+                              </option>
+                              <option value='1'>Indian</option>
+                              <option value='2'>Foreigner</option>
+                            </select>
+                          </div>
+                          <div className='form-input'>
+                            <h4>Total Amount: </h4>
+                            <div className='total-amount'>₹ {totalAmount}</div>
+                          </div>
+                        </form>
+                      </div> */}
                     </Modal.Body>
-                    {/* <Modal.Footer>
-                      <Button variant='secondary' onClick={handleClose}>
-                        Close
-                      </Button>
-                      <Button variant='primary' onClick={handleClose}>
-                        Save Changes
-                      </Button>
-                    </Modal.Footer> */}
                   </Modal>
 
                   <div className='row align-items-center'>
